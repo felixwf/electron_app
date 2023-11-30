aaaeedc9fdbd28eb62c3280163c0307f408f6fc5
felix <felix@viewer.tech>
2023-11-30 18:51:54 +0800

    init prj

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/node_modules/electron`)
+  });
+  
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
